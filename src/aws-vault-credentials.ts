642eb55ef2d99ddbc957915f7f15a5f7554347c5
Gordon Leigh <gordonmleigh@gmail.com>
2024-01-28 20:08:35 +0100

    fix module exports

--- src/aws-vault-credentials.ts
+++ src/aws-vault-credentials.ts
@@ -1,4 +1,4 @@
-import { Credentials, Provider } from "@aws-sdk/types";
+import type { Credentials, Provider } from "@aws-sdk/types";
 import { spawn } from "child_process";
 import { joinConcurrentCallers } from "./join-concurrent-callers.js";
 
